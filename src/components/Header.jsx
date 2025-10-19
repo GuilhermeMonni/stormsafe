e7c072eb863e2dd4bb38cdeff87a92714964bac0
Guilherme Monni <gmonni20@gmail.com>
2025-10-19 11:06:35 -0300

    fix header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,11 @@ function Header(){
 
     return(
     <>
-        <div className="flex justify-center w-full h-28 bg-sky-950">
-            <img src="/imgs/logo.png" alt="Logo StormSafe" className="w-1/9 object-cover" />
+        <div className="flex justify-center items-center w-full h-28 bg-sky-950">
+            <div className="flex flex-col justify-center items-center w-1/2 text-center h-full py-2">
+                <img src="/imgs/logo.png" alt="Logo StormSafe" className="-mt-3 h-28 w-auto object-contain" />
+                <p className="-mt-6 text-sm text-gray-50">O Seu Algoritmo de Informação e Meteorologia</p>
+            </div>
         </div>
     </>
     )
