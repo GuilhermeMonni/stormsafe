b1b4a4ffe53ebf8289a729a48a8a2480f362d3c1
Guilherme Monni <gmonni20@gmail.com>
2025-10-19 10:26:31 -0300

    add climate div

---
+++ src/components/Climate.jsx
@@ -0,0 +1,17 @@
+//import { useState } from "react";
+
+function Climate(){
+
+    return(
+    <>
+        <div className="w-1/5 h-56 bg-indigo-300 rounded-lg">
+            dados climaticos
+        </div>
+    </>
+    )
+}
+
+export default Climate
+
+
+
--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,7 +1,10 @@
+import Climate from './Climate'
+
 function Container() {
     return(
         <main className="bg-indigo-200 w-full h-screen">
             conteudo
+            <Climate />
         </main>
     )
 }
