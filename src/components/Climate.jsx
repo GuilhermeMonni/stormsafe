6c992300a2c873a3849ada7b22f7b604caba6c45
Guilherme Monni <gmonni20@gmail.com>
2025-10-20 21:39:54 -0300

    search api finish

--- src/components/Climate.jsx
+++ src/components/Climate.jsx
@@ -1,6 +1,39 @@
-//import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function Climate(){
+    let [lat, setLat] = useState(null) //latitude user
+    let [lon, setLon] = useState(null) //longitude user
+
+    useEffect(() => { //set lat e lon user
+        if(navigator.geolocation){ //search location use
+        navigator.geolocation.getCurrentPosition(
+            (pos) => {
+                setLat(pos.coords.latitude)
+                setLon(pos.coords.longitude)
+            },
+            (err) => {
+                console.error('Erro ao obter localização', err.message)
+            }
+        )
+        }
+    }, [])
+
+    useEffect(() => { //search infos
+        if(lat !== null && lon !== null){
+            searchClimate()
+        }
+    }, [lat, lon])
+
+    async function searchClimate() {
+        try{ //search infos
+           const res = await fetch(`https://stormsafe-api.onrender.com/climate/${lat}/${lon}`)
+
+           const data = await res.json()
+           console.log(data)
+        } catch(err){
+            console.error('Erro ao buscar cidade.', err)
+        }
+    }
 
     return(
     <>
