4aa96aac2a6d6cc43e87f95a4bf17ce4eed884d7
Guilherme Monni <gmonni20@gmail.com>
2025-10-23 20:47:55 -0300

    fix li climate

--- src/components/Climate.jsx
+++ src/components/Climate.jsx
@@ -3,13 +3,22 @@ import { useState, useEffect } from "react";
 function Climate(){
     let [lat, setLat] = useState(null) //latitude user
     let [lon, setLon] = useState(null) //longitude user
+    let [climateData, setClimateData] = useState(null) //infos climate
+    let [weatherData, setWeatherData] = useState({
+        condition: '',
+        temperature: 0, 
+        humidity: 0
+    })
 
     useEffect(() => { //set lat e lon user
         if(navigator.geolocation){ //search location use
         navigator.geolocation.getCurrentPosition(
             (pos) => {
-                setLat(pos.coords.latitude)
-                setLon(pos.coords.longitude)
+                const lat = parseFloat(pos.coords.latitude).toFixed(6)
+                const lon = parseFloat(pos.coords.longitude).toFixed(6)
+
+                setLat(lat)
+                setLon(lon)
             },
             (err) => {
                 console.error('Erro ao obter localização', err.message)
@@ -21,10 +30,11 @@ function Climate(){
     useEffect(() => { //search infos
         async function searchClimate() {
         try{ //search infos
-           const res = await fetch(`https://stormsafe-api.onrender.com/climate/${lat}/${lon}`)
+           const res = await fetch(`https://stormsafe-api.onrender.com/climate/${lat}/${lon}`) //url api
 
            const data = await res.json()
-           return data
+           console.log(data)
+           setClimateData(data) //set data api
         } catch(err){
             console.error('Erro ao buscar cidade.', err)
         }
@@ -35,10 +45,26 @@ function Climate(){
         }
     }, [lat, lon])
 
+    useEffect(() => {
+        if(climateData){
+            setWeatherData({
+                condition: climateData.condition,
+                text: climateData.text,
+                temp: 'Temperatura: ' + parseInt(climateData.temp) + '°C',
+                humidity: 'Umidade: ' + climateData.humidity
+            })
+        }
+    }, [climateData])
+
     return(
     <>
         <div className="w-1/5 h-56 bg-indigo-300 rounded-lg">
-            dados climaticos
+            <h2 key="title-city">{climateData ? climateData.city : 'Carregando...'}</h2>
+            <ul>
+                <li key="condition">{weatherData.text}</li>
+                <li key="temperature">{weatherData.temp}</li>
+                <li key="humidity">{weatherData.humidity}</li>
+            </ul>
         </div>
     </>
     )
