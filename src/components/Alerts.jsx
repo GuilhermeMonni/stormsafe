4e973b9075ca84829e9d60af42aaf9ec671474be
Guilherme Monni <gmonni20@gmail.com>
2025-11-01 19:29:51 -0300

    add alerts

---
+++ src/components/Alerts.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect, createElement } from "react"
+
+function Alerts(){
+    let [dataAlerts, setDataAlerts] = useState({
+        id: '',
+        title: '',
+        link: '',
+        descripion: 0,
+        img: ''
+    })//return api 
+    let [loading, setLoading] = useState(true) //loading page status
+
+    useEffect(() => {
+        async function searchNews(){
+            try{
+                const response = await fetch('https://stormsafe-api.onrender.com/alerts')
+
+                const dataResponse = await response.json()
+                console.log(dataResponse.dataResponse)
+                setDataAlerts(dataResponse.dataResponse)
+            }finally{
+                setLoading(false)
+            }
+        }
+
+        searchNews()     
+    }, [])
+
+    if(loading) return <p>Carregando...</p>
+
+    return(
+        <>
+            {dataAlerts.map((alert) => (
+                <article key={alert.id}>
+                    <h2>{alert.title}</h2>
+                    <p>{alert.description}</p>
+                    <img src={alert.img} alt={alert.title}></img>
+                    <a href={alert.link}>Ler mais</a>
+                </article>
+            ))}
+        </>
+    )
+}
+
+export default Alerts
--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,10 +1,11 @@
 import Climate from './Climate'
+import Alerts from './Alerts'
 
 function Container() {
     return(
         <main className="bg-indigo-200 w-full h-screen">
             <Climate />
-            conteudo
+            <Alerts />
         </main>
     )
 }
